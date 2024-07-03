14f6bfd9dd0af42e4870acbe4e727f95e3c71a56
jooyoung <hjoo820@naver.com>
2024-07-03 14:53:37 +0900

    WrongInputLength

--- Project10/Baseball.cpp
+++ Project10/Baseball.cpp
@@ -1,6 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 class Game {
-
+public : 
+	void guess(const string& str) {
+		if (str.length() != 3)
+			throw length_error("Must be 3 letters");
+	}
 };
--- Sample-Test1/test.cpp
+++ Sample-Test1/test.cpp
@@ -1,7 +1,9 @@
 #include "pch.h"
 #include "../Project10/Baseball.cpp"
+#include <string>
+using namespace std;
 
-TEST(TestCaseName, TestName) {
-  EXPECT_EQ(1, 1);
-
+TEST(BaseballGame, ThrowExceptionWhenWrongInputLength) {
+	Game game;
+	EXPECT_THROW(game.guess(string("12")), length_error);
 }
